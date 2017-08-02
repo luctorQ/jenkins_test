ec88489fe317905046727817fb14086e966104c4
Pawel <luchtort@gmail.com>
2017-08-02 23:57:50 +0100

    Create Jenkinsfile.go

---
+++ Jenkinsfile.go
@@ -0,0 +1,47 @@
+pipeline {
+  agent any
+  stages {
+    stage('stage0') {
+      steps {
+        parallel(
+          "stage66": {
+            echo 'hello'
+            
+          },
+          "stage0_1": {
+            echo 'stage1'
+            
+          },
+          "error": {
+            isUnix()
+            sleep 5
+            
+          }
+        )
+      }
+    }
+    stage('stage3') {
+      steps {
+        echo 'dddd'
+      }
+    }
+    stage('hgsag') {
+      steps {
+        parallel(
+          "hgsagdd": {
+            echo 'safasfd'
+            echo 'asfasfdddd'
+            
+          },
+          "gsdafas": {
+            echo 'asfasfdsa'
+            
+          }
+        )
+      }
+    }
+  }
+  environment {
+    dsaf = '33'
+  }
+}

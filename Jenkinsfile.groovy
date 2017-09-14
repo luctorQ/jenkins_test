dd71a032c24f8a1d09483fdf5222d4cf46233888
PLUSZYNSKI <pluszynski@bleak.pl>
2017-09-14 16:14:22 +0100

    create file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,11 @@
 import hudson.tasks.junit.TestResultAction;
 import groovy.text.SimpleTemplateEngine
 
+def statusBuild=[
+	b1:[status:'undefined',msg:'Build status undefined'],
+	b2:[status:'undefined',msg:'Build status undefined']
+]
+
 pipeline {
 	agent any
 	stages {
@@ -8,36 +13,42 @@ pipeline {
 			steps {
 				script{
 					def build1= build job: 'java_project1',
-								propagate: false
+					propagate: false
+					
+					buildStatus.b1.status=build1.result;
+//					buildStatus.b1.msg=build1.rawBuild.writeWholeLogTo()
+					def logFile=build1.rawBuild.logFile;
+					def dest=new File(pwd(),'b1_dest.log')
+					dest.write(logFile.text)
+					
 					println "build1:"+build1
-					
 					println 'actions:'+build1.rawBuild.actions
-					
+/*
 					println 'pwd:'+pwd()
-					
+
 					def templateFile = new File(pwd(),"templates/simple.template")
 					def engine = new SimpleTemplateEngine()
 					def template = engine.createTemplate(templateFile)
-					
+
 					def data=[name:'testowo']
-					
+
 					def writable = template.make(data)
-					println 'from template:'+writable
-					
+					println 'from template:'+writable*/
+
 					def testsResult=build1.rawBuild.getAction(hudson.tasks.test.AggregatedTestResultAction)
-//					def testsResult=build1.getRawBuild().getAction(hudson.tasks.junit.TestResultAction.class)
-					
-					
-					
+					//					def testsResult=build1.getRawBuild().getAction(hudson.tasks.junit.TestResultAction.class)
+
+
+
 					if (testsResult == null) {
 						println("No tests")
-					  }else {
-						  println('tests failed count:'+testsResult.failCount)
-						  println('tests skipped count:'+testsResult.skipCount)
-						  println('tests total count:'+testsResult.totalCount)
-						  testsResult=null;
-					  }
-					
+					}else {
+						println('tests failed count:'+testsResult.failCount)
+						println('tests skipped count:'+testsResult.skipCount)
+						println('tests total count:'+testsResult.totalCount)
+						testsResult=null;
+					}
+
 
 					def response2= build job: 'java_project2'
 					//			  propagate: false
@@ -45,9 +56,7 @@ pipeline {
 				}
 			}
 		}
-		stage('reporting') {
-			steps { echo 'reporting' }
-		}
+		stage('reporting') { steps { echo 'reporting' } }
 	}
 	environment { dsaf = '33' }
 	post{

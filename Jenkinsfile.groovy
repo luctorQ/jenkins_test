04e3f7db62c5c6ea0f05b4455cb0da524a55d86c
PLUSZYNSKI <pluszynski@bleak.pl>
2017-09-14 13:00:17 +0100

    formatted

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,39 +1,36 @@
 import java.util.stream.Streams.AbstractStreamBuilderImpl
 
 pipeline {
-  agent any
-  stages {
-    stage('build') {
-      steps {
-		  script{
-			  def response1= build job: 'java_project1',
-//			  propagate: false
-			  println "response1:"+response1
-			  
-			  def response2= build job: 'java_project2',
-//			  propagate: false
-			  println "response2:"+response2
-		  }
-		  
-      }
-    }
-    stage('reporting') {
-      steps {
-        echo 'reporting'
-      }
-    }
-  }
-  environment {
-    dsaf = '33'
-  }
-  post{
-	  echo 'post actions'
-	  
-	  emailext to: 'luchtort@gmail.com',
-	  replyTo: 'pluszynski@bleak.pl',
-	  subject: "test email local",
-	  body: """
+	agent any
+	stages {
+		stage('build') {
+			steps {
+				script{
+					def response1= build job: 'java_project1',
+					//			  propagate: false
+					println "response1:"+response1
+
+					def response2= build job: 'java_project2',
+					//			  propagate: false
+					println "response2:"+response2
+				}
+			}
+		}
+		stage('reporting') {
+			steps { echo 'reporting' }
+		}
+	}
+	environment { dsaf = '33' }
+	post{
+		always{
+			echo 'post actions'
+
+			emailext to: 'luchtort@gmail.com',
+			replyTo: 'pluszynski@bleak.pl',
+			subject: "test email local",
+			body: """
 			local test
         """
-  }
+		}
+	}
 }

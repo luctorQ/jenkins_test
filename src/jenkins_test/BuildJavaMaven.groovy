36b82ddf4751629c1c85303315fdc63a37d3197c
PLUSZYNSKI <pluszynski@bleak.pl>
2017-09-26 12:37:46 +0100

    mvn all

--- src/jenkins_test/BuildJavaMaven.groovy
+++ src/jenkins_test/BuildJavaMaven.groovy
@@ -1,8 +1,22 @@
 package jenkins_test
 
 node() {
-	
+
 	stage('build'){
 		echo 'build stage'
+
+		git url: 'https://github.com/luctorQ/java_project1.git/'
+
+		withMaven(
+				// Maven installation declared in the Jenkins "Global Tool Configuration"
+				maven: 'M3',
+				// Maven settings.xml file defined with the Jenkins Config File Provider Plugin
+				// Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
+				mavenSettingsConfig: 'my-maven-settings',
+				mavenLocalRepo: '.repository') {
+				 // Run the maven build
+					sh "mvn clean install" 
+				} // withMaven will discover the generated Maven artifacts, JUnit Surefire & FailSafe reports and FindBugs reports
+
 	}
 }

d65f963c1204ab38a7807fb2ee26895e27815a85
PLUSZYNSKI <pluszynski@bleak.pl>
2017-09-26 12:55:16 +0100

    m3

--- src/jenkins_test/BuildJavaMaven.groovy
+++ src/jenkins_test/BuildJavaMaven.groovy
@@ -9,10 +9,10 @@ node() {
 
 		withMaven(
 				// Maven installation declared in the Jenkins "Global Tool Configuration"
-				maven: 'M3',
+				maven: 'apache-maven-3.3.9',
 				// Maven settings.xml file defined with the Jenkins Config File Provider Plugin
 				// Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
-				mavenSettingsConfig: 'my-maven-settings',
+//				mavenSettingsConfig: 'my-maven-settings',
 				mavenLocalRepo: '.repository') {
 				 // Run the maven build
 					sh "mvn clean install" 

78f545e5699c655e956a7329538ef2704d9a6846
PLUSZYNSKI <pluszynski@bleak.pl>
2017-09-26 13:26:56 +0100

    bat mvn

--- src/jenkins_test/BuildJavaMaven.groovy
+++ src/jenkins_test/BuildJavaMaven.groovy
@@ -13,10 +13,11 @@ node() {
 				// Maven settings.xml file defined with the Jenkins Config File Provider Plugin
 				// Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
 //				mavenSettingsConfig: 'my-maven-settings',
-				mavenLocalRepo: '.repository') {
+				mavenLocalRepo: '.repository'
+		) {
 				 // Run the maven build
-					sh "mvn clean install" 
-				} // withMaven will discover the generated Maven artifacts, JUnit Surefire & FailSafe reports and FindBugs reports
+					bat "mvn clean install" 
+		} // withMaven will discover the generated Maven artifacts, JUnit Surefire & FailSafe reports and FindBugs reports
 
 	}
 }

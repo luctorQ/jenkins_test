d8375fe16011233106d7f47aaa336728f714be8c
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-20 14:36:11 +0100

    remote

--- Jenkins_pipeline_lib.groovy
+++ Jenkins_pipeline_lib.groovy
@@ -1,4 +1,9 @@
-@Library('test-lib') _
+//@Library('test-lib') _
+
+@Library(identifier: 'test-lib@master', retriever: modernSCM(
+  [$class: 'GitSCMSource',
+   remote: 'https://github.com/luctorQ/jenkins_shared_lib.git',
+   credentialsId: '8f8ed4c0-b044-44f2-8029-66964ab226d2']))
  
 pipeline {
     agent any

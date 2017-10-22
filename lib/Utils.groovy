cdc4e935701ec7b4b2cf2e9c6835c5f15754ee24
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-22 15:12:32 +0100

    ds

--- lib/Utils.groovy
+++ lib/Utils.groovy
@@ -5,8 +5,8 @@ import javax.swing.GraphicsWrapper
 import groovy.transform.Field
 
 APPS_DICT=[PC:'pc',BC:'bc',CC:'cc',AB:'ab'] 
-class Context implements Serializable{
-//	List<Revision> requestedRevisions=new List<Revision>()
+class BuildContext implements Serializable{
+	List<Revision> requestedRevisions=new List<Revision>()
 
 /*	class Revision implements Serializable{
 		String appname

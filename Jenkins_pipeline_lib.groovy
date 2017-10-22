f34a532de7866e6d86f2da7b9d49004d97382672
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-22 09:48:28 +0100

    test

--- Jenkins_pipeline_lib.groovy
+++ Jenkins_pipeline_lib.groovy
@@ -13,9 +13,16 @@ library(identifier: 'test-lib@master', retriever: modernSCM(
    remote: 'https://github.com/luctorQ/jenkins_shared_lib.git',
    credentialsId: '8f8ed4c0-b044-44f2-8029-66964ab226d2']))
  
+
+def PipelineUtils
 pipeline {
     agent any
     stages {
+		stage("Initialize"){
+			PipelineUtils=load('lib/PipelineUtils.groovy')
+			def pu=new PipelineUtils()
+			pu.calculateName()
+		}
         stage('Show quote') {
             steps {
                 showQuote()
---
+++ lib/PipelineUtils.groovy
@@ -0,0 +1,9 @@
+package lib
+
+class PipelineUtils {
+	def name
+	
+	String calculateName() {
+		return "Hello: ${name}"
+	}
+}

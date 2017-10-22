eefc695d8e96e0e197d5b92d64ad332ecdb848eb
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-22 09:49:42 +0100

    d

--- Jenkins_pipeline_lib.groovy
+++ Jenkins_pipeline_lib.groovy
@@ -9,25 +9,28 @@ println 'params:'+params
 println 'thia:'+this
 
 library(identifier: 'test-lib@master', retriever: modernSCM(
-  [$class: 'GitSCMSource',
-   remote: 'https://github.com/luctorQ/jenkins_shared_lib.git',
-   credentialsId: '8f8ed4c0-b044-44f2-8029-66964ab226d2']))
- 
+[$class: 'GitSCMSource',
+	remote: 'https://github.com/luctorQ/jenkins_shared_lib.git',
+	credentialsId: '8f8ed4c0-b044-44f2-8029-66964ab226d2']))
+
 
 def PipelineUtils
 pipeline {
-    agent any
-    stages {
+	agent any
+	stages {
 		stage("Initialize"){
-			PipelineUtils=load('lib/PipelineUtils.groovy')
-			def pu=new PipelineUtils()
-			pu.calculateName()
+			script{
+				PipelineUtils=load('lib/PipelineUtils.groovy')
+				println 'PipelineUtils:'+PipelineUtils
+				def pu=new PipelineUtils()
+				pu.calculateName()
+			}
 		}
-        stage('Show quote') {
-            steps {
-                showQuote()
+		stage('Show quote') {
+			steps {
+				showQuote()
 				showSharedQuote()
-            }
-        }
-    }
+			}
+		}
+	}
 }

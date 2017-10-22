c44a33d4393b4ca0637c6458f3a5a3797deb39c8
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-22 13:53:31 +0100

    enums

--- lib/Utils.groovy
+++ lib/Utils.groovy
@@ -1,6 +1,33 @@
 package lib
 
+import javax.swing.GraphicsWrapper
+
 import groovy.transform.Field
+
+enum GW_APPS{
+	PC,BC,CC,CM
+	
+	GW_APPS(int value) {
+		this.value = value
+	}
+	private final int value
+	
+	int getValue() {
+		value
+	}
+}
+
+class BuildInfo implements Serializable{
+	Integer svnrevision
+	Integer jobid
+	
+	BuildInfo(svnrevision,jobid) {
+		this.svnrevision=svnrevision
+		this.jobid=jobid
+	}
+	
+	
+}
 
 class Context implements Serializable {
 	
@@ -38,6 +65,12 @@ def runU2() {
 	u2.showName()
 	Context ctx=new Context();
 	ctx.revbc=443
+	
+	String pc='PC'
+	
+	def enumed=pc as GW_APPS
+	println 'enumed:'+enumed
+	
 	
 }
 

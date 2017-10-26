c1faeb875f53c48f5dd93f7f7ef991e12b23e485
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-26 09:14:51 +0100

    asddsa

--- Jenkins_pipeline_lib.groovy
+++ Jenkins_pipeline_lib.groovy
@@ -34,9 +34,9 @@ List props = []
 
 def groovyscript="""
 import org.boon.Boon;
-//import org.jenkinsci.plugins.workflow.libs.Library
+import org.jenkinsci.plugins.workflow.libs.Library
 
-//@Library('test-lib') _
+@Library('test-lib') _
 import com.hastingsdirect.ExtendedProperties;
 
 def jsonEditorOptions = Boon.fromJson(/{

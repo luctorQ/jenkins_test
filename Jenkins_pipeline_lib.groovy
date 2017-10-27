6f529447aaaaee68e1de501e72ee725f515fd815
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-27 10:58:11 +0100

    refactoring params

--- Jenkins_pipeline_lib.groovy
+++ Jenkins_pipeline_lib.groovy
@@ -123,56 +123,20 @@ def jsonEditorOptions = Boon.fromJson(/{
 return jsonEditorOptions
 """
 
-com.cwctravel.hudson.plugins.extended_choice_parameter.ExtendedChoiceParameterDefinition test1=ExtendedProperties.createJsonField('JSPARAM',groovyscript,'NEWPARAM')
+def jsonParam=ExtendedProperties.createJsonField('JSPARAM',groovyscript,'NEWPARAM')
 
-com.cwctravel.hudson.plugins.extended_choice_parameter.ExtendedChoiceParameterDefinition test = new com.cwctravel.hudson.plugins.extended_choice_parameter.ExtendedChoiceParameterDefinition(
-	"JSONPARAM", //name
-	"PT_JSON",//type
-	null,//value
-	null,//project name
-	null,//property file
-	groovyscript,//groovy script
-	null,
-	null,// bindings
-	"c:/tmp/libs_jenkins_global/jenkins_shared_lib.jar",//groovyclasspath
-	//"c:/Users/PLUSZYNSKI/.jenkins/plugins/workflow-cps-global-lib/WEB-INF/lib/workflow-cps-global-lib.jar",
-//	"c:/Users/PLUSZYNSKI/.jenkins/jobs/pipeline_test_libs/workspace@libs/test-lib/src",//groovyclasspath
-	null, // propertykey
-	null,//"VALUE, B", //default value
-	null,
-	null,
-	null,
-	null, //default bindings
-	null,
-	null,
-	null, //descriptionPropertyValue
-	null,
-	null,
-	null,
-	null,
-	null,
-	null,
-	null,// javascript file
-	null, // javascript
-	false, // save json param to file
-	false, // quote
-	2, // visible item count
-	"DESC", //description
-	","
-)
-
-params1 << test1
-//props << parameters(params)
+params1 << jsonParam
 
 println 'paramsddddd:'+params1
 
 
 properties([parameters([
 				string(name: 'BRANCH', defaultValue: 'master'),
-				string(name: 'BRANCH_1', defaultValue: 'master')
+				string(name: 'BRANCH_1', defaultValue: 'master'),
+				jsonParam
 			])])
 
-properties([parameters(params1)])
+//properties([parameters(params1)])
 
 pipeline {
 	agent any

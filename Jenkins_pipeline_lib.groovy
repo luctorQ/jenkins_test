12b3e6e9e1215f0a32c8f52e75ea94c0293e9a77
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-27 13:42:48 +0100

    dd

--- Jenkins_pipeline_lib.groovy
+++ Jenkins_pipeline_lib.groovy
@@ -117,7 +117,7 @@ return jsonEditorOptions
 properties([parameters([
 				string(name: 'BRANCH', defaultValue: 'master'),
 				string(name: 'BRANCH_1', defaultValue: 'master'),
-				ep.extendedChoiceParam(name:'JSON_PARAM',groovyScript:groovyscript,description:'Descr')
+				extendedChoiceParam(name:'JSON_PARAM',groovyScript:groovyscript,description:'Descr')
 			])])
 
 

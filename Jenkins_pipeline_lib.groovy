949d07a966e509b9f8611c486ad2918120d76061
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-28 08:44:42 +0100

    ds

--- Jenkins_pipeline_lib.groovy
+++ Jenkins_pipeline_lib.groovy
@@ -22,7 +22,7 @@ properties([
 	parameters([
 		string(name: 'BRANCH', defaultValue: 'master'),
 		string(name: 'BRANCH_1', defaultValue: 'master'),
-		//				extendedChoiceParam(name:'JSON_PARAM',groovyScript:groovyscript,description:'Descr'),
+						extendedChoiceParam(name:'JSON_PARAM',groovyScript:groovyscript,description:'Descr'),
 		extendedChoiceParam(new EPPromotedBuilds("TEST_PARAM",'Promoted builds'))
 	])
 ])

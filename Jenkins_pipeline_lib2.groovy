9f1b41c70d64fdab0a280d0e6604a344cbe01857
pluszynski <pluszynski@bleak.pl>
2017-11-19 19:24:17 +0000

    ds

--- Jenkins_pipeline_lib2.groovy
+++ Jenkins_pipeline_lib2.groovy
@@ -11,17 +11,6 @@ println 'env.WORKSPACE:'+env.WORKSPACE
 
 println 'params:'+params
 
-properties([
-	parameters([
-		string(name: 'BRANCH', defaultValue: 'master'),
-		string(name: 'BRANCH_1', defaultValue: 'master'),
-		//						extendedChoiceParam(name:'JSON_PARAM',groovyScript:groovyscript,description:'Descr'),
-		extendedChoiceParam(new EPPromotedBuilds("TEST_PARAM",'Promoted builds')),
-		extendedChoiceParam(new EPPromotedBuilds("TEST_PARAM2",'Promoted builds'))
-	])
-])
-
-
 pipeline {
 	agent any
 	stages {

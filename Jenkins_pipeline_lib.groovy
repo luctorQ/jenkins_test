5a3417f020275304e30f44428eacf15ec27abbd4
pluszynski <pluszynski@bleak.pl>
2017-11-22 16:26:21 +0000

    sadf

--- Jenkins_pipeline_lib.groovy
+++ Jenkins_pipeline_lib.groovy
@@ -43,11 +43,11 @@ pipeline {
 					def build=getBuild("pipeline_test_libs2",325)
 					println 'BUILD'+build.getClass()
 
-/*					def mytemplate=new Template()
-					def str=mytemplate.eval('emailtemplate.groovy')
+					def mytemplate=new Template()
+					def str=mytemplate.eval('com/hastingsdirect/templates/emailtemplate.groovy')
 				
 					println 'after str:'+str	
-*/				
+				
 					
 					
 					def tpl=sendEmail("ddd")

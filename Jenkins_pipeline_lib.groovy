8824e4998699f61a7f26111178cde2b61c20f35c
pluszynski <pluszynski@bleak.pl>
2017-12-11 12:39:41 +0000

    fds

--- Jenkins_pipeline_lib.groovy
+++ Jenkins_pipeline_lib.groovy
@@ -387,7 +387,7 @@ pipeline {
 					sendEmail(template:'templates/email-build-deploy-summary.groovy',
 					recipients:'pluszynski@bleak.pl',
 					bindings:[
-						SVN_BRANCH:params.SVN_BRANCH,
+						SVN_BRANCH:params.BRANCH,
 						HISTORY_EVENTS:HISTORY_EVENTS
 					]
 					)

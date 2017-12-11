0f240228f5034857360a241c6f717ca4155b61fb
pluszynski <pluszynski@bleak.pl>
2017-12-11 11:56:18 +0000

    sadf

--- src/jenkins_test/TestsScript.groovy
+++ src/jenkins_test/TestsScript.groovy
@@ -1,12 +1,12 @@
-package jenkins_test
+LATEST_SVN_REVISIONS=[ab:26399, bc:26662, pc:26659, cc:26659]
+def predicted_revisions=[ab:26399, bc:26615, cc:26632, pc:26611, ec:25718]
 
-def test=[aa:'okok']
+def matchLatestRevisions=LATEST_SVN_REVISIONS.inject(true,{result,entry->
+    entry.value==predicted_revisions[entry.key] && result
+})
+println 'matchLatestRevisions:'+matchLatestRevisions
 
-println test?test:'nothing'
-
-def method(first,opt='UNDEFINED') {
-	println ('first:'+first+' opt:'+(opt?:'abc'))
+ABC=[]
+if(ABC.size()>0 || !matchLatestRevisions) {
+	println 'go'
 }
-
-
-method(12,null)
